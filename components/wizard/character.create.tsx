84234078447c5fd08078ccc44f139cba51ddb762
jollygrin <lol@dean.lol>
2023-08-13 00:35:39 +0200

    fix/next-image-allowlisting

--- components/wizard/character.create.tsx
+++ components/wizard/character.create.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { FC } from "react";
 import Image from "next/image";
+import { getLadImageURL } from "@/lib/apiClient";
 
 export const CharGen: FC<{ back: () => void; next: () => void }> = ({
   next: nextStep,
@@ -10,12 +11,7 @@ export const CharGen: FC<{ back: () => void; next: () => void }> = ({
     <>
       <Flex direction="column" justifyContent="space-between" minH="60vh">
         <Box>
-          <Image
-            width="200"
-            height="200"
-            alt="lad"
-            src="https://www.madlads.com/_next/image?url=https%3A%2F%2Fmadlads.s3.us-west-2.amazonaws.com%2Fimages%2F22.png&w=640&q=75"
-          />
+          <Image width="200" height="200" alt="lad" src={getLadImageURL(10)} />
         </Box>
         <Flex gap="2rem">
           <Button variant="outline" w="100%" alignSelf="end" onClick={backStep}>
