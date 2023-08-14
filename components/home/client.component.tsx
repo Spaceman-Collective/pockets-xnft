c595ff28636110c4d09b5c0244012acf4bec2d6c
jollygrin <lol@dean.lol>
2023-08-14 02:17:31 +0200

    fix build

--- components/home/client.component.tsx
+++ components/home/client.component.tsx
@@ -7,36 +7,36 @@ export default function Home() {
     useWeb3Auth();
   return (
     <>
-      <Box>
-        <Button
-          bg={web3auth?.connected ? "green" : "red"}
-          isDisabled={web3auth?.connected}
-          onClick={async () => {
-            await login();
-          }}
-        >
-          connect
-        </Button>
+      {/* <Box> */}
+      {/*   <Button */}
+      {/*     bg={web3auth?.connected ? "green" : "red"} */}
+      {/*     isDisabled={web3auth?.connected} */}
+      {/*     onClick={async () => { */}
+      {/*       await login(); */}
+      {/*     }} */}
+      {/*   > */}
+      {/*     connect */}
+      {/*   </Button> */}
 
-        <Button
-          bg={!!authIdToken ? "green" : "red"}
-          isDisabled={!!authIdToken}
-          onClick={async () => {
-            await authenticateUser();
-          }}
-        >
-          authenticateUser
-        </Button>
+      {/*   <Button */}
+      {/*     bg={!!authIdToken ? "green" : "red"} */}
+      {/*     isDisabled={!!authIdToken} */}
+      {/*     onClick={async () => { */}
+      {/*       await authenticateUser(); */}
+      {/*     }} */}
+      {/*   > */}
+      {/*     authenticateUser */}
+      {/*   </Button> */}
 
-        <Button
-          onClick={async () => {
-            await signTransaction();
-          }}
-        >
-          sign
-        </Button>
-        <Text>{authIdToken?.idToken}</Text>
-      </Box>
+      {/*   <Button */}
+      {/*     onClick={async () => { */}
+      {/*       // await signTransaction(); */}
+      {/*     }} */}
+      {/*   > */}
+      {/*     sign */}
+      {/*   </Button> */}
+      {/*   <Text>{authIdToken?.idToken}</Text> */}
+      {/* </Box> */}
     </>
   );
 }
