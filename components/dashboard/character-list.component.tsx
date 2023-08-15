988e93fdf7ec1deb2396f13bb90fd57401144ce6
jollygrin <JollyGrin@protonmail.com>
2023-08-15 21:14:01 +0200

    styling refactor

--- components/dashboard/character-list.component.tsx
+++ components/dashboard/character-list.component.tsx
@@ -25,16 +25,17 @@ export const CharacterList: FC<{ data?: Character[] }> = ({ data }) => {
         <Flex direction='column' gap='1rem' my='1rem'>
           {data?.map(char => {
             return (
-              <Flex key={char.mint} flex='1 auto 1' justifyContent='space-between' p='1rem' bg='blacks.500' borderRadius='1rem'>
-                <Frame img={char.image} size='8rem' />
-                <Flex direction='column'>
-                  <Text>
-                    {char.name}
-                  </Text>
-
-                  <Text>
-                    XP:
-                  </Text>
+              <Flex key={char.mint} flex='auto 1 auto' justifyContent='space-between' p='1rem' bg='blacks.500' borderRadius='1rem'>
+                <Flex gap='1rem' alignItems='end'>
+                  <Frame img={char.image} size='8rem' />
+                  <Flex direction='column'>
+                    <Text fontSize='2.25rem' letterSpacing='1px'>
+                      {char.name.split(' ')[0]}
+                    </Text>
+                    <Text fontSize='1.75rem' fontWeight={700} letterSpacing='1px' textTransform='uppercase'>
+                      {char.name.split(' ')[1]}
+                    </Text>
+                  </Flex>
                 </Flex>
                 <Box h='5rem' w='5rem' bg='black' borderRadius='1rem' backgroundImage='https://picsum.photos/200' backgroundSize='cover' backgroundPosition='center' />
               </Flex>
