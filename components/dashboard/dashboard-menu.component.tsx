1865278f069b6774b441b9c464e013dd26b1e93b
Adrian Petrykiw <apetry2@illinois.edu>
2023-08-17 11:47:43 -0500

    added character page

--- components/dashboard/dashboard-menu.component.tsx
+++ components/dashboard/dashboard-menu.component.tsx
@@ -37,6 +37,21 @@ export const DashboardMenu = () => {
         h="6rem"
         flex="1"
         ml="1rem"
+        onClick={() => router.push("/character")}
+      >
+        CHARACTER
+      </Button>
+      <Button
+        bg={colors.brand.primary}
+        color={router.pathname === "/faction" ? colors.brand.quaternary : colors.brand.secondary}        
+        borderRadius="0.5rem"
+        p="0.25rem"
+        fontSize="18px"
+        fontWeight={600}
+        letterSpacing="3px"
+        h="6rem"
+        flex="1"
+        ml="1rem"
         onClick={() => router.push("/faction")}
       >
         FACTION
