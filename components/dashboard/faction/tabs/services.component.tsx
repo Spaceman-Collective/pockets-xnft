92723f240120d84e9e1def17873916a0822e70a7
jollygrin <JollyGrin@protonmail.com>
2023-08-18 18:42:37 +0200

    style refactor

--- components/dashboard/faction/tabs/services.component.tsx
+++ components/dashboard/faction/tabs/services.component.tsx
@@ -9,60 +9,16 @@ export const FactionTabServices: React.FC<{
   currentCharacter: Character;
   setFactionStatus: (value: boolean) => void;
 }> = ({ currentCharacter, setFactionStatus }) => {
+  console.log({ currentCharacter });
   return (
-    <PanelContainer display="flex" flexDirection="column" gap="1rem">
-      {/* <Header factionName={currentCharacter?.faction?.name} /> */}
-      <ServicesGrid />
-    </PanelContainer>
-  );
-};
-
-const Header: React.FC<{ factionName: string | undefined }> = ({
-  factionName,
-}) => {
-  return <Title verticalAlign="end">{factionName}</Title>;
-};
-
-const HeaderStats = () => {
-  return (
-    <Flex mt="2rem" pr="1rem" w="100%" justifyContent="space-between">
-      <TownHall />
-      <Flex direction="column" justifyContent="space-between" alignItems="end">
-        <Title verticalAlign="end">{"faction name"}</Title>
-        <HStack alignItems="end" justifySelf="end" pb="2rem" gap="1rem">
-          <Label>Level</Label>
-          <Value>12</Value>
-          <Label>Population</Label>
-          <Value>12</Value>
-        </HStack>
-      </Flex>
-    </Flex>
-  );
-};
-
-const TownHall = () => {
-  const size = 17;
-  return (
-    <Box
-      bg="blacks.700"
-      minH={size + "rem"}
-      w={size + 1.5 + "rem"} // adjust for padding removed from bottom
-      p="1.5rem 1.5rem 0"
-      justifySelf="start"
-      borderRadius="1rem 1rem 0 0"
-      alignItems="end"
-    >
-      <Station />
-    </Box>
-  );
-};
-const ServicesGrid = () => {
-  return (
-    <Box>
-      <HeaderStats />
+    <PanelContainer display="flex" flexDirection="column">
+      <HeaderStats
+        factionName={currentCharacter.faction?.name}
+        description={currentCharacter?.faction?.description}
+      />
       <Grid
         bg="blacks.700"
-        p="3rem"
+        p="1.5rem"
         gap="1rem"
         borderRadius="0 1rem 1rem 1rem"
         templateColumns={`repeat(auto-fill, minmax(${stationSize}, 1fr))`}
@@ -81,6 +37,59 @@ const ServicesGrid = () => {
           ),
         )}
       </Grid>
+    </PanelContainer>
+  );
+};
+
+const HeaderStats: React.FC<{
+  factionName: string | undefined;
+  description: string | undefined;
+}> = ({ factionName, description }) => {
+  return (
+    <Flex mt="2rem" w="100%">
+      <TownHall />
+      <Flex
+        direction="column"
+        justifyContent="space-between"
+        alignItems="start"
+        w="100%"
+        p="1rem 1.5rem"
+        flexGrow={1}
+      >
+        <VStack alignItems="start">
+          <Title>{factionName}</Title>
+          <Text noOfLines={3} textOverflow="ellipsis">
+            {description}
+          </Text>
+        </VStack>
+        <HStack alignSelf="end" justifySelf="end" gap="2rem">
+          <Flex alignItems="center" gap="0.5rem">
+            <Label>Level</Label>
+            <Value>12</Value>
+          </Flex>
+          <Flex alignItems="center" gap="0.5rem">
+            <Label>Population</Label>
+            <Value>12</Value>
+          </Flex>
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+};
+
+const TownHall = () => {
+  const size = 17;
+  return (
+    <Box
+      bg="blacks.700"
+      h={size + "rem"}
+      w={size + 6 + "rem"} // adjust for padding removed from bottom
+      p="1.5rem 1.5rem 0"
+      justifySelf="start"
+      borderRadius="1rem 1rem 0 0"
+      alignSelf="end"
+    >
+      <Station />
     </Box>
   );
 };
