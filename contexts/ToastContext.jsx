51bfd63829ff41a5090ffacbe0bf8628bc7c0422
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-08-17 23:49:15 -0500

    fix: remove types

--- contexts/ToastContext.jsx
+++ contexts/ToastContext.jsx
@@ -12,7 +12,7 @@ const initialState = {
 export const ToastContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(toastReducer, initialState);
 
-  const addToast = (type: string, message: string) => {
+  const addToast = (type, message) => {
     const id = Math.floor(Math.random() * 10000000);
     dispatch({ type: "ADD_TOAST", payload: { id, message, type } });
   };
