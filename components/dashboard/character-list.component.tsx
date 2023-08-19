b2f59e641787e41da99bc0543c1459a70e2b8ac9
jollygrin <lol@dean.lol>
2023-08-19 04:03:44 +0200

    add skeleton to faction char loading

--- components/dashboard/character-list.component.tsx
+++ components/dashboard/character-list.component.tsx
@@ -1,22 +1,26 @@
-import { FC,  useState } from "react";
+import { FC, useState } from "react";
 import { useRouter } from "next/router";
-import { Flex, Button, Text, Box } from "@chakra-ui/react";
-import {
-  CharacterListContainer,
-} from "@/components/Containers.styled";
+import { Flex, Button, Text, Box, Skeleton } from "@chakra-ui/react";
+import { CharacterListContainer } from "@/components/Containers.styled";
 import { Character } from "@/types/server";
 import { Frame } from "../wizard/wizard.components";
 import styled from "@emotion/styled";
 
 import { colors } from "@/styles/defaultTheme";
 
-interface Props  {
-  selectedCharacter: Character | undefined | null,
-  setSelectedCharacter: (char?: Character | null) => void,
-  data?: Character[]
+interface Props {
+  selectedCharacter: Character | undefined | null;
+  setSelectedCharacter: (char?: Character | null) => void;
+  data?: Character[];
+  isLoading?: boolean;
 }
 
-export const CharacterList: FC<Props> = ({ selectedCharacter, setSelectedCharacter, data }: Props) => {
+export const CharacterList: FC<Props> = ({
+  selectedCharacter,
+  setSelectedCharacter,
+  data,
+  isLoading,
+}: Props) => {
   const router = useRouter();
   const [actionStatus, setActionStatus] = useState(false);
 
@@ -26,7 +30,7 @@ export const CharacterList: FC<Props> = ({ selectedCharacter, setSelectedCharact
     } else {
       setSelectedCharacter(char);
     }
-  }
+  };
 
   return (
     <CharacterListContainer>
@@ -35,33 +39,59 @@ export const CharacterList: FC<Props> = ({ selectedCharacter, setSelectedCharact
         border="2px solid"
         borderColor={colors.blacks[700]}
         _hover={{
-          borderColor: colors.blacks[700]
+          borderColor: colors.blacks[700],
         }}
         width="100%"
         onClick={() => router.push("/wizard")}
       >
         Character +
       </Button>
-      <Flex direction='column' gap='1rem' my='1rem'>
-          {data?.map(char => {
-            return (
-              <CharacterFlex key={char.mint} onClick={() => handleCharacterSelect(char)} selected={char.mint === selectedCharacter?.mint}>
-                <Flex gap='1rem' alignItems='center'>
-                  <Frame img={char.image} size='8rem' />
-                  <Flex direction='column'>
-                    <Text fontSize='2.25rem' letterSpacing='1px'>
-                      {char.name.split(' ')[0]}
-                    </Text>
-                    <Text fontSize='1.75rem' fontWeight={700} letterSpacing='1px' textTransform='uppercase'>
-                      {char.name.split(' ')[1]}
-                    </Text>
-                  </Flex>
+      <Flex direction="column" gap="1rem" my="1rem">
+        {isLoading &&
+          Array.from({ length: 4 }).map((_, i) => (
+            <Skeleton
+              key={"charskele" + i}
+              h="10rem"
+              w="100%"
+              borderRadius="1rem"
+            />
+          ))}
+        {data?.map((char) => {
+          return (
+            <CharacterFlex
+              key={char.mint}
+              onClick={() => handleCharacterSelect(char)}
+              selected={char.mint === selectedCharacter?.mint}
+            >
+              <Flex gap="1rem" alignItems="center">
+                <Frame img={char.image} size="8rem" />
+                <Flex direction="column">
+                  <Text fontSize="2.25rem" letterSpacing="1px">
+                    {char.name.split(" ")[0]}
+                  </Text>
+                  <Text
+                    fontSize="1.75rem"
+                    fontWeight={700}
+                    letterSpacing="1px"
+                    textTransform="uppercase"
+                  >
+                    {char.name.split(" ")[1]}
+                  </Text>
                 </Flex>
-                <Box h='4rem' w='4rem' bg='black' borderRadius='1rem' backgroundImage='' backgroundSize='cover' backgroundPosition='center' />
-              </CharacterFlex>
-            )
-          })}
-        </Flex>
+              </Flex>
+              <Box
+                h="4rem"
+                w="4rem"
+                bg="black"
+                borderRadius="1rem"
+                backgroundImage=""
+                backgroundSize="cover"
+                backgroundPosition="center"
+              />
+            </CharacterFlex>
+          );
+        })}
+      </Flex>
     </CharacterListContainer>
   );
 };
@@ -74,7 +104,8 @@ const CharacterFlex = styled(Flex)<{ selected?: boolean }>`
   cursor: pointer;
   transition: background-color 0.3s;
   background-color: ${colors.blacks[500]};
-  border: 2px solid ${(props) => {
-    return props.selected ? colors.brand.secondary : colors.blacks[500]
-  }};
+  border: 2px solid
+    ${(props) => {
+      return props.selected ? colors.brand.secondary : colors.blacks[500];
+    }};
 `;
