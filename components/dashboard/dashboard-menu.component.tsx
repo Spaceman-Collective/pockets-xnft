040f560d3b31d248361572e90a577c442fa348b8
jollygrin <lol@dean.lol>
2023-08-24 01:58:51 +0200

    align dashboard menu item

--- components/dashboard/dashboard-menu.component.tsx
+++ components/dashboard/dashboard-menu.component.tsx
@@ -6,29 +6,34 @@ export const DashboardMenu = () => {
   const router = useRouter();
 
   return (
-    <Box
-      cursor="pointer"
-      display="flex"
-      justifyContent="space-between"
-    >
+    <Box cursor="pointer" display="flex" justifyContent="space-between">
       <Button
         bg={colors.brand.primary}
-        color={router.pathname === "/" ? colors.brand.quaternary : colors.brand.secondary}
+        color={
+          router.pathname === "/"
+            ? colors.brand.quaternary
+            : colors.brand.secondary
+        }
         borderRadius="0.5rem"
         p="0.25rem"
         fontSize="18px"
         fontWeight={600}
         letterSpacing="3px"
         h="6rem"
-        flex="1" 
+        flex="1"
         mr="1rem"
+        minW="38rem"
         onClick={() => router.push("/")}
       >
         WALLET
       </Button>
       <Button
         bg={colors.brand.primary}
-        color={router.pathname === "/character" ? colors.brand.quaternary : colors.brand.secondary}        
+        color={
+          router.pathname === "/character"
+            ? colors.brand.quaternary
+            : colors.brand.secondary
+        }
         borderRadius="0.5rem"
         p="0.25rem"
         fontSize="18px"
@@ -44,7 +49,11 @@ export const DashboardMenu = () => {
       </Button>
       <Button
         bg={colors.brand.primary}
-        color={router.pathname === "/faction" ? colors.brand.quaternary : colors.brand.secondary}        
+        color={
+          router.pathname === "/faction"
+            ? colors.brand.quaternary
+            : colors.brand.secondary
+        }
         borderRadius="0.5rem"
         p="0.25rem"
         fontSize="18px"
