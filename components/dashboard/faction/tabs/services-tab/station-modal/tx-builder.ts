b76e6eb0a8e26d88aa7daf0d2c0a44bdceef9e12
jollygrin <lol@dean.lol>
2023-08-24 00:08:38 +0200

    refactor the big web3 function

---
+++ components/dashboard/faction/tabs/services-tab/station-modal/tx-builder.ts
@@ -0,0 +1,95 @@
+import { BONK_MINT, RESOURCES, STATION_USE_COST_PER_LEVEL } from "@/constants";
+import { Connection, TransactionInstruction } from "@solana/web3.js";
+import { toast } from "react-hot-toast";
+import { Blueprint } from "../constants";
+import { QueryClient, UseMutateFunction } from "@tanstack/react-query";
+import { Faction } from "@/types/server";
+
+type Props = {
+  startCountdown: () => void;
+  buildTransferIx: (prop: any) => TransactionInstruction;
+  buildMemoIx: (prop: any) => TransactionInstruction;
+  buildBurnIx: (prop: any) => TransactionInstruction;
+  mutateStartStation: (prop: any, handle: any) => any;
+  encodeTransaction: (prop: any) => Promise<any | undefined>;
+  stationBlueprint?: Blueprint;
+  signTransaction: any;
+  connection: Connection;
+  walletAddress?: string;
+  selectedCharacter: any;
+  station: any;
+  queryClient: QueryClient;
+};
+
+const startStationProcess = async ({
+  connection,
+  walletAddress,
+  selectedCharacter,
+  station,
+  stationBlueprint,
+  signTransaction,
+  encodeTransaction,
+  buildMemoIx,
+  buildTransferIx,
+  buildBurnIx,
+  mutateStartStation,
+  startCountdown,
+  queryClient,
+}: Props) => {
+  if (!walletAddress) return toast.error("No wallet connected");
+  const ix = buildMemoIx({
+    walletAddress,
+    payload: {
+      mint: selectedCharacter?.mint,
+      timestamp: Date.now().toString(),
+      stationId: station?.id,
+    },
+  });
+
+  const bonkIx = buildTransferIx({
+    walletAddress,
+    mint: BONK_MINT.toString(),
+    amount: STATION_USE_COST_PER_LEVEL * BigInt(station?.level!),
+    decimals: 5,
+  });
+
+  const burnIxs = stationBlueprint?.inputs?.map((e) => {
+    return buildBurnIx({
+      walletAddress,
+      mint: RESOURCES.find((r) => r.name == e.resource)?.mint as string,
+      amount: BigInt(e.amount),
+      decimals: 0,
+    });
+  });
+
+  if (!burnIxs || burnIxs.length === 0 || burnIxs instanceof Error)
+    return toast.error("Ooops! No burnIx");
+  try {
+    const encodedTx = await encodeTransaction({
+      walletAddress,
+      connection,
+      signTransaction,
+      txInstructions: [ix, bonkIx, ...burnIxs],
+    });
+
+    if (encodedTx instanceof Error || encodedTx === undefined)
+      return toast.error("Failed to start station");
+
+    mutateStartStation(
+      { signedTx: encodedTx },
+      {
+        onSuccess: () => {
+          queryClient.refetchQueries({ queryKey: ["char-timers"] });
+          queryClient.refetchQueries({ queryKey: ["assets"] });
+          startCountdown();
+          toast.success("You've started a build in the " + station?.blueprint);
+        },
+        onError: (e: any) => {
+          toast.error("Ooops! Did not start station: \n\n" + e);
+        },
+      },
+    );
+  } catch (err) {
+    toast.error("Oops! That didn't work: \n\n" + JSON.stringify(err));
+  }
+};
