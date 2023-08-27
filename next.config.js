74776a130075f22c231d5dbd75123d186cc154ed
jollygrin <lol@dean.lol>
2023-08-27 04:11:02 +0200

    fix honey

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "content.honey.land",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
