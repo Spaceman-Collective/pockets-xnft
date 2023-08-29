cd4dc6fd1eef15be7a9e760a4bab5a8fed8ecbb2
spacemandev <devbharel@gmail.com>
2023-08-28 20:38:04 -0500

    changed initialClaimaint to check characterMint

--- components/dashboard/faction/tabs/resources-tab/prospect-modal.component.tsx
+++ components/dashboard/faction/tabs/resources-tab/prospect-modal.component.tsx
@@ -25,7 +25,7 @@ import { useRfAllocate } from "@/hooks/useRf";
 import { Character } from "@/types/server";
 import { timeout } from "@/lib/utils";
 
-const tickets = [ 1, 5, 10, 50, 100 ];
+const tickets = [1, 5, 10, 50, 100];
 const MAX_NUM_IX = 20;
 
 type RFAccount = {
@@ -45,15 +45,24 @@ export const ModalRfProspect: FC<{
   factionId?: string;
   refetchRF?: any;
   currentCharacter: Character;
-  fire: () => void
-}> = ({ isOpen, onClose, rf, charMint: characterMint, factionId, refetchRF, currentCharacter, fire: fireConfetti }) => {
+  fire: () => void;
+}> = ({
+  isOpen,
+  onClose,
+  rf,
+  charMint: characterMint,
+  factionId,
+  refetchRF,
+  currentCharacter,
+  fire: fireConfetti,
+}) => {
   const {
     walletAddress,
     connection,
     buildProspectIx,
     getRFAccount,
     sendAllTransactions,
-    signAllTransactions
+    signAllTransactions,
   } = useSolana();
 
   const { mutate } = useRfAllocate();
@@ -64,16 +73,16 @@ export const ModalRfProspect: FC<{
   const [prospectLoading, setProspectLoading] = useState<boolean>(false);
   const [numProspectTickets, setNumProspectTickets] = useState<number>(0);
 
-
-
   const refreshRFAccount = useCallback(async () => {
     if (!rf?.id) return console.error("NO  ACCOUNT ID", rf);
 
     try {
       const account = await getRFAccount(connection, rf?.id);
-      console.log('rfa account: ', account)
+      console.log("rfa account: ", account);
       setRfAccount(account as RFAccount);
-      const hitJackpot = account.isHarvestable && account?.initialClaimant?.toString() === walletAddress
+      const hitJackpot =
+        account.isHarvestable &&
+        account?.initialClaimant?.toString() === characterMint;
       setJackpot(hitJackpot);
     } catch (err) {
       console.error(err);
@@ -87,28 +96,21 @@ export const ModalRfProspect: FC<{
   const handleJackpot = async () => {
     try {
       setClaimLoading(true);
-      mutate(
-        { signedTx: undefined, charMint: currentCharacter!.mint },
-      );
+      mutate({ signedTx: undefined, charMint: currentCharacter!.mint });
     } catch (err) {
       console.error(err);
     } finally {
       /* For better UX - hold on a second before closing */
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      toast.success('JACKPOT!!!! RF Claimed')
+      toast.success("JACKPOT!!!! RF Claimed");
       setClaimLoading(false);
-      fireConfetti()
+      fireConfetti();
       onClose();
     }
-  }
+  };
 
   const post = async () => {
-    if (
-      !characterMint ||
-      !factionId ||
-      !rf?.id ||
-      !walletAddress
-    ) {
+    if (!characterMint || !factionId || !rf?.id || !walletAddress) {
       toast.error("Something is not right!");
       return;
     }
@@ -140,8 +142,16 @@ export const ModalRfProspect: FC<{
 
         for (txIdx = 0; txIdx < numTransactions; txIdx++) {
           try {
-            let txSlice = allTxs.slice(txIdx + (txIdx > 0 ? 1 : 0) * MAX_NUM_IX, ((txIdx + 1) * MAX_NUM_IX));
-            let sigs = await sendAllTransactions(connection, txSlice, walletAddress, signAllTransactions);
+            let txSlice = allTxs.slice(
+              txIdx + (txIdx > 0 ? 1 : 0) * MAX_NUM_IX,
+              (txIdx + 1) * MAX_NUM_IX
+            );
+            let sigs = await sendAllTransactions(
+              connection,
+              txSlice,
+              walletAddress,
+              signAllTransactions
+            );
             sigArr.push(...sigs!);
           } catch (err) {
             console.error(err);
@@ -149,12 +159,25 @@ export const ModalRfProspect: FC<{
         }
 
         if (remTransactions > 0) {
-          let remainderTxSlice = allTxs.slice((txIdx * MAX_NUM_IX) - (txIdx > 0 ? 1 : 0), allTxs.length);
-          let sigs = await sendAllTransactions(connection, remainderTxSlice, walletAddress, signAllTransactions);
+          let remainderTxSlice = allTxs.slice(
+            txIdx * MAX_NUM_IX - (txIdx > 0 ? 1 : 0),
+            allTxs.length
+          );
+          let sigs = await sendAllTransactions(
+            connection,
+            remainderTxSlice,
+            walletAddress,
+            signAllTransactions
+          );
           sigArr.push(...sigs!);
         }
       } else {
-        let sigs = await sendAllTransactions(connection, allTxs, walletAddress, signAllTransactions);
+        let sigs = await sendAllTransactions(
+          connection,
+          allTxs,
+          walletAddress,
+          signAllTransactions
+        );
         sigArr.push(...sigs!);
       }
 
@@ -182,85 +205,99 @@ export const ModalRfProspect: FC<{
         minW="40vw"
         minH="40vh"
       >
-        <ModalHeader fontSize="24px" fontWeight="bold" letterSpacing="3px">Prospect Resource Field</ModalHeader>
+        <ModalHeader fontSize="24px" fontWeight="bold" letterSpacing="3px">
+          Prospect Resource Field
+        </ModalHeader>
         <ModalCloseButton display={{ base: "inline", md: "none" }} />
-        <ModalBody display={'flex'} flexDirection={'column'} justifyContent={'space-between'}>
-          <Text>Take a chance to claim this resource field. Even if you fail, you increase development of the resource field, increasing the chance for the next transaction to win.</Text>
-            {jackpot ? (
-              <VStack gap={4}>
-                <Text textAlign={'center'}>
-                  Congrats, anon! You scored the lucky ticket. You can now claim the resource field for your faction.
-                </Text>
-                <Button isLoading={claimLoading} onClick={handleJackpot}>Claim</Button>
-              </VStack>
-            ) : (
-              <VStack pt='28'>
-                <Text>Choose number of tickets</Text>
-                <Flex gap={"6"} justifyContent={"center"} my="2rem">
-                  <Button
-                    w="24"
-                    onClick={() => {
-                      if (numProspectTickets > 0)
-                        setNumProspectTickets(numProspectTickets - 1);
-                    }}
-                  >
-                    -
-                  </Button>
-                  <StyledInput
-                    value={numProspectTickets}
-                    type="number"
-                    onChange={(event) =>
-                      setNumProspectTickets(Number(event.target.value))
-                    }
-                  />
-                  <Button
-                    w="24"
-                    onClick={() =>
-                      setNumProspectTickets(numProspectTickets + 1)
-                    }
-                  >
-                    +
-                  </Button>
-                </Flex>
-                <Flex gap={"6"} justifyContent={"center"} flexDir={"row"}>
-                  {tickets.map((ticket) => (
-                    <Button
-                      size={"md"}
-                      key={ticket}
-                      onClick={() => setNumProspectTickets(ticket)}
-                    >
-                      {ticket}
-                    </Button>
-                  ))}
-                </Flex>
-
-                <Flex w='100%' gap={3}>
-                  <Text pb='4' pt='8'>Your Txs:</Text>
-                  {signedArr
-                    ?.filter((val, index) => index < 4)
-                    .map((sig, index) => (
-                      <Link
-                        key={`sig-${index}`}
-                        href={`https://solscan.io/tx/${sig}`}
-                        target="_blank"
-                      >
-                        <StyledText>
-                          {sig.slice(0, 4)}...{sig.slice(sig.length - 4, sig.length)}
-                        </StyledText>
-                      </Link>
-                    ))}
-                </Flex>
-
+        <ModalBody
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"space-between"}
+        >
+          <Text>
+            Take a chance to claim this resource field. Even if you fail, you
+            increase development of the resource field, increasing the chance
+            for the next transaction to win.
+          </Text>
+          {jackpot ? (
+            <VStack gap={4}>
+              <Text textAlign={"center"}>
+                Congrats, anon! You scored the lucky ticket. You can now claim
+                the resource field for your faction.
+              </Text>
+              <Button isLoading={claimLoading} onClick={handleJackpot}>
+                Claim
+              </Button>
+            </VStack>
+          ) : (
+            <VStack pt="28">
+              <Text>Choose number of tickets</Text>
+              <Flex gap={"6"} justifyContent={"center"} my="2rem">
                 <Button
-                  isLoading={prospectLoading}
-                  isDisabled={jackpot}
-                  w="100%"
-                  onClick={post}
+                  w="24"
+                  onClick={() => {
+                    if (numProspectTickets > 0)
+                      setNumProspectTickets(numProspectTickets - 1);
+                  }}
                 >
-                  Prospect
+                  -
                 </Button>
-              </VStack>
-            )}
+                <StyledInput
+                  value={numProspectTickets}
+                  type="number"
+                  onChange={(event) =>
+                    setNumProspectTickets(Number(event.target.value))
+                  }
+                />
+                <Button
+                  w="24"
+                  onClick={() => setNumProspectTickets(numProspectTickets + 1)}
+                >
+                  +
+                </Button>
+              </Flex>
+              <Flex gap={"6"} justifyContent={"center"} flexDir={"row"}>
+                {tickets.map((ticket) => (
+                  <Button
+                    size={"md"}
+                    key={ticket}
+                    onClick={() => setNumProspectTickets(ticket)}
+                  >
+                    {ticket}
+                  </Button>
+                ))}
+              </Flex>
+
+              <Flex w="100%" gap={3}>
+                <Text pb="4" pt="8">
+                  Your Txs:
+                </Text>
+                {signedArr
+                  ?.filter((val, index) => index < 4)
+                  .map((sig, index) => (
+                    <Link
+                      key={`sig-${index}`}
+                      href={`https://solscan.io/tx/${sig}`}
+                      target="_blank"
+                    >
+                      <StyledText>
+                        {sig.slice(0, 4)}...
+                        {sig.slice(sig.length - 4, sig.length)}
+                      </StyledText>
+                    </Link>
+                  ))}
+              </Flex>
+
+              <Button
+                isLoading={prospectLoading}
+                isDisabled={jackpot}
+                w="100%"
+                onClick={post}
+              >
+                Prospect
+              </Button>
+            </VStack>
+          )}
         </ModalBody>
       </ModalContent>
     </Modal>
