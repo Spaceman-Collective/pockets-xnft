9e677794c893be99471fbcbd384117e3eaa89fef
Adrian Petrykiw <apetry2@illinois.edu>
2023-09-02 15:03:46 -0500

    eslint

--- components/dashboard/faction/tabs/politics-tab/index.tsx
+++ components/dashboard/faction/tabs/politics-tab/index.tsx
@@ -225,7 +225,7 @@ export const FactionTabPolitics: React.FC<FactionTabPoliticsProps> = ({
 								color="brand.secondary"
 								cursor="pointer"
 								_hover={{
-									textDecoration: "underline"
+									textDecoration: "underline",
 								}}
 								onClick={reclaimProposalVotes}
 							>
