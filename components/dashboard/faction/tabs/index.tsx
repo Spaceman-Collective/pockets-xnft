31e1adc9257c50dbeb00253573871de5442bce11
Adrian Petrykiw <apetry2@illinois.edu>
2023-09-02 14:44:21 -0500

    yarn build

--- components/dashboard/faction/tabs/index.tsx
+++ components/dashboard/faction/tabs/index.tsx
@@ -12,48 +12,52 @@ export const FactionTabs: React.FC<{
 	currentCharacter: Character
 	setFactionStatus: (value: boolean) => void
 }> = ({ currentCharacter, setFactionStatus }) => {
-  const citizenDisclosure = useDisclosure()
-  const factionId = currentCharacter?.faction?.id ?? ""
-  const { data: factionData } = useFaction({ factionId })
+	const citizenDisclosure = useDisclosure()
+	const factionId = currentCharacter?.faction?.id ?? ""
+	const { data: factionData } = useFaction({ factionId })
 
-  useEffect(() => {
-    setFactionStatus(!!currentCharacter?.faction);
-  }, [currentCharacter?.faction, setFactionStatus]);
+	useEffect(() => {
+		setFactionStatus(!!currentCharacter?.faction)
+	}, [currentCharacter?.faction, setFactionStatus])
 
-  return (
-    <>
-      {factionData?.citizens && (
-        <CitizenModal {...citizenDisclosure} citizens={factionData.citizens} setFactionStatus={setFactionStatus} />
-      )}
-      <PageTabs
-        tabItems={[
-          {
-            tabName: "Resources",
-            Component: FactionTabResources,
-            componentProps: {
-              currentCharacter: currentCharacter,
-              setFactionStatus: setFactionStatus,
-            },
-          },
-          {
-            tabName: "Services",
-            Component: FactionTabServices,
-            componentProps: {
-              currentCharacter: currentCharacter,
-              openCitizenModal: citizenDisclosure.onOpen,
-            },
-          },
-          {
-            tabName: "Politics",
-            Component: FactionTabPolitics,
-            componentProps: {
-              currentCharacter: currentCharacter,
-              setFactionStatus: setFactionStatus,
-              openCitizenModal: citizenDisclosure.onOpen,
-            },
-          },
-        ]}
-      />
-    </>
-  )
+	return (
+		<>
+			{factionData?.citizens && (
+				<CitizenModal
+					{...citizenDisclosure}
+					citizens={factionData.citizens}
+					setFactionStatus={setFactionStatus}
+				/>
+			)}
+			<PageTabs
+				tabItems={[
+					{
+						tabName: "Resources",
+						Component: FactionTabResources,
+						componentProps: {
+							currentCharacter: currentCharacter,
+							setFactionStatus: setFactionStatus,
+						},
+					},
+					{
+						tabName: "Services",
+						Component: FactionTabServices,
+						componentProps: {
+							currentCharacter: currentCharacter,
+							openCitizenModal: citizenDisclosure.onOpen,
+						},
+					},
+					{
+						tabName: "Politics",
+						Component: FactionTabPolitics,
+						componentProps: {
+							currentCharacter: currentCharacter,
+							setFactionStatus: setFactionStatus,
+							openCitizenModal: citizenDisclosure.onOpen,
+						},
+					},
+				]}
+			/>
+		</>
+	)
 }
