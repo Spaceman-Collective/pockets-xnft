1a5ec4b1bc5ab0ab8c4c82fe1ee604286a8a0706
jollygrin <lol@dean.lol>
2023-09-04 00:13:13 +0200

    replace selectedchar with router

--- hooks/useSelectedCharacter.ts
+++ hooks/useSelectedCharacter.ts
@@ -1,32 +1,53 @@
 import { Character } from "@/types/server"
+import { useRouter } from "next/router"
 import { useCallback, useEffect, useState } from "react"
-
-const storedCharKey = "SELECTEDCHARACTER"
+import { useAssets } from "./useCharacters"
 
 export const useSelectedCharacter = (): [
 	Character | null | undefined,
 	(char?: Character | null) => void,
 ] => {
+	const { data: assets } = useAssets()
+	const router = useRouter()
+
 	const [selectedCharacter, setSelectedCharacter] = useState<
 		Character | null | undefined
 	>(null)
 
-	const setSelectedChar = useCallback((character?: Character | null) => {
-		if (character) {
-			sessionStorage.setItem(storedCharKey, JSON.stringify(character))
-		} else {
-			sessionStorage.removeItem(storedCharKey)
-		}
+	const setSelectedChar = useCallback(
+		(character?: Character | null) => {
+			if (character) {
+				router.push({
+					query: {
+						char: character.mint,
+					},
+				})
+			} else {
+				const charMint = router.query?.char
+				if (!charMint || charMint === undefined) return
+				const existingQuery = router.query
+				const { char, ...rest } = existingQuery
+				router.push({
+					query: { ...rest },
+				})
+			}
 
-		setSelectedCharacter(character)
-	}, [])
+			setSelectedCharacter(character)
+		},
+		[router],
+	)
 
 	useEffect(() => {
-		const char = sessionStorage.getItem(storedCharKey)
+		const charMint = router.query?.char
+		if (!assets || assets === undefined) return
+		if (!charMint || charMint === undefined) return
+		if (!assets?.characters || assets?.characters === undefined) return
+		const char = assets.characters.find((e) => e.mint === charMint)
+
 		if (char) {
-			setSelectedCharacter(JSON.parse(char))
+			setSelectedCharacter(char)
 		}
-	}, [])
+	}, [assets, router.query?.char])
 
 	return [selectedCharacter, setSelectedChar]
 }
